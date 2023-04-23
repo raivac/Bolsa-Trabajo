483acee0b068aafa4a9d29036107df4c81cfd5f6
raivac <raulivars1997@gmail.com>
2023-04-23 12:02:32 +0200

    registros empresa/candidato hechos

--- frontend/src/app/auth/registro-candidato.component.ts
+++ frontend/src/app/auth/registro-candidato.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { CreateCandidatoDto } from '../models/create-candidato.dto';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { CreateEmpresaDto } from '../models/create-empresa.dto';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-registro-candidato',
@@ -6,5 +11,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./registro-candidato.component.css']
 })
 export class RegistroCandidatoComponent {
+  nuevoCandidato: CreateCandidatoDto = new CreateCandidatoDto('', '','','',0,'');
+  email: string = "";
+  password!: string;
+  nombre: string= ""; 
+  apellidos: string= ""; 
+  telefono: number= 0; 
+  cif: string= ""; 
 
+  constructor(
+    private authService: AuthService,
+    private router: Router
+
+  ) { }
+  ngOnInit(): void {
+  }
+  onRegister():void{
+    this.nuevoCandidato = new CreateCandidatoDto(this.email,this.password,this.nombre,this.apellidos,this.telefono,this.cif);
+    this.authService.registroCandidato(this.nuevoCandidato).subscribe(
+      data=>{
+        Swal.fire({
+          icon: 'success',
+          title: 'Candidato registrado correctamente ',
+          showConfirmButton: false,
+          timer: 3000
+        });
+        this.router.navigate(['/login-candidato'])
+      },
+      err =>{
+        console.log(err);
+        Swal.fire({
+          icon: 'error',
+          title: 'Esta cuenta ya existe',
+          showConfirmButton: false,
+          timer: 3000
+        });
+      }
+    )
+  }
 }
--- frontend/src/app/auth/registro-empresa.component.ts
+++ frontend/src/app/auth/registro-empresa.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CreateEmpresaDto } from '../models/create-empresa.dto';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-registro-empresa',
   templateUrl: './registro-empresa.component.html',
   styleUrls: ['./registro-empresa.component.css']
 })
-export class RegistroEmpresaComponent {
+export class RegistroEmpresaComponent  implements OnInit{
+  nuevaEmpresa: CreateEmpresaDto = new CreateEmpresaDto('', '','','','',0,'','');
+  email: string = "";
+  password!: string;
+  nombre: string= ""; 
+  apellidos: string= ""; 
+  empresa: string= ""; 
+  telefono: number= 0; 
+  cif: string= ""; 
+  descripcion: string= ""
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+
+  ) { }
+  ngOnInit(): void {
+  }
+  onRegister():void{
+    this.nuevaEmpresa = new CreateEmpresaDto(this.email,this.password,this.nombre,this.apellidos,this.empresa,this.telefono,this.cif,this.descripcion);
+    this.authService.registroEmpresa(this.nuevaEmpresa).subscribe(
+      data=>{
+        Swal.fire({
+          icon: 'success',
+          title: 'Empresa registrada correctamente ',
+          showConfirmButton: false,
+          timer: 3000
+        });
+        this.router.navigate(['/login-empresa'])
+      },
+      err =>{
+        console.log(err);
+        Swal.fire({
+          icon: 'error',
+          title: 'Esta cuenta ya existe',
+          showConfirmButton: false,
+          timer: 3000
+        });
+      }
+    )
+  }
 
 }
