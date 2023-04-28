dd36834b3723e6507acae59fd7ef93241249f6e0
raivac <raulivars1997@gmail.com>
2023-04-28 19:23:41 +0200

    frontend reorganizado

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListaEmpleoComponent } from './empleo/lista-empleo.component';
-import { DetalleEmpleoComponent } from './empleo/detalle-empleo.component';
-import { NuevoEmpleoComponent } from './empleo/nuevo-empleo.component';
-import { EditarEmpleoComponent } from './empleo/editar-empleo.component';
-import { PoliticaComponent } from './empleo/politica.component';
-import { LoginCandidatoComponent } from './auth/login-candidato.component';
-import { LoginEmpresaComponent } from './auth/login-empresa.component';
-import { RegistroCandidatoComponent } from './auth/registro-candidato.component';
-import { RegistroEmpresaComponent } from './auth/registro-empresa.component';
+import { ListaEmpleoComponent } from './empleo/lista/lista-empleo.component';
+import { DetalleEmpleoComponent } from './empleo/detalle/detalle-empleo.component';
+import { NuevoEmpleoComponent } from './empleo/nuevo/nuevo-empleo.component';
+import { EditarEmpleoComponent } from './empleo/editar/editar-empleo.component';
+import { PoliticaComponent } from './empleo/politica/politica.component';
+import { LoginCandidatoComponent } from './auth/loginCandidato/login-candidato.component';
+import { LoginEmpresaComponent } from './auth/loginEmpresa/login-empresa.component';
+import { RegistroCandidatoComponent } from './auth/registroCandidato/registro-candidato.component';
+import { RegistroEmpresaComponent } from './auth/registroEmpresa/registro-empresa.component';
 import { LoginEmpresaGuard } from './guards/login-empresa.guard';
 import { LoginCandidatoGuard } from './guards/login-candidato.guard';
-import { MisOfertasComponent } from './empleo/mis-ofertas.component';
+import { MisOfertasComponent } from './empleo/misOfertas/mis-ofertas.component';
 import { EmpleoGuard } from './guards/empleo.guard';
 import { UserGuard } from './guards/user.guard';
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,21 +4,20 @@ import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListaEmpleoComponent } from './empleo/lista-empleo.component';
-import { NuevoEmpleoComponent } from './empleo/nuevo-empleo.component';
-import { EditarEmpleoComponent } from './empleo/editar-empleo.component';
-import { DetalleEmpleoComponent } from './empleo/detalle-empleo.component';
-import { PoliticaComponent } from './empleo/politica.component';
+import { ListaEmpleoComponent } from './empleo/lista/lista-empleo.component';
+import { NuevoEmpleoComponent } from './empleo/nuevo/nuevo-empleo.component'; 
+import { EditarEmpleoComponent } from './empleo/editar/editar-empleo.component'; 
+import { DetalleEmpleoComponent } from './empleo/detalle/detalle-empleo.component'; 
+import { PoliticaComponent } from './empleo/politica/politica.component'; 
 import { MenuComponent } from './menu/menu.component';
-import { LoginCandidatoComponent } from './auth/login-candidato.component';
-import { LoginEmpresaComponent } from './auth/login-empresa.component';
-import { RegistroEmpresaComponent } from './auth/registro-empresa.component';
-import { RegistroCandidatoComponent } from './auth/registro-candidato.component';
+import { LoginCandidatoComponent } from './auth/loginCandidato/login-candidato.component';
+import { LoginEmpresaComponent } from './auth/loginEmpresa/login-empresa.component';
+import { RegistroEmpresaComponent } from './auth/registroEmpresa/registro-empresa.component';
+import { RegistroCandidatoComponent } from './auth/registroCandidato/registro-candidato.component';
 import { interceptorProvider } from './interceptors/empleo.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
-import { MisOfertasComponent } from './empleo/mis-ofertas.component';
-
+import { MisOfertasComponent } from './empleo/misOfertas/mis-ofertas.component';
 
 
 @NgModule({
