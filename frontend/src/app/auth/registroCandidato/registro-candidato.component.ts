abc3d203946064443460efdeb4093ae1f73999e9
raivac <raulivars1997@gmail.com>
2023-04-28 19:44:56 +0200

    registros restringidos

--- frontend/src/app/auth/registroCandidato/registro-candidato.component.ts
+++ frontend/src/app/auth/registroCandidato/registro-candidato.component.ts
@@ -3,6 +3,7 @@ import { CreateCandidatoDto } from '../../models/create-candidato.dto';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
 import { AuthService } from '../../services/auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-registro-candidato',
@@ -17,15 +18,47 @@ export class RegistroCandidatoComponent {
   apellidos: string= ""; 
   telefono: number= 0; 
   cif: string= ""; 
+  valid: boolean = true;
 
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private toastr: ToastrService,
 
   ) { }
   ngOnInit(): void {
   }
   onRegister():void{
+    this.valid = true;
+
+    if (!this.nombre) {
+      this.toastr.error('El nombre es obligatorio', 'Error');
+      this.valid = false;
+    }
+    if (!this.apellidos) {
+      this.toastr.error('Los apellidos son', 'Error');
+      this.valid = false;
+    }
+    if (!this.password) {
+      this.toastr.error('La contraseña es obligatoria', 'Error');
+      this.valid = false;
+    }
+    if (!this.cif || !/^\d{8}[A-HJ-NP-TV-Z]$/i.test(this.cif)) {
+      this.toastr.error('El DNI es obligatorio y debe ser válido', 'Error');
+      this.valid = false;
+    }
+
+    if (!this.telefono || !/^[679][0-9]{8}$/.test(this.telefono.toString())) {
+      this.toastr.error('El teléfono es obligatorio y debe ser válido', 'Error');
+      this.valid = false;
+    }    
+    
+    if (!this.email || !/\S+@\S+\.\S+/.test(this.email)) {
+      this.toastr.error('El email es obligatorio y debe ser válido', 'Error');
+      this.valid = false;
+    }
+
+    if (this.valid) {
     this.nuevoCandidato = new CreateCandidatoDto(this.email,this.password,this.nombre,this.apellidos,this.telefono,this.cif);
     this.authService.registroCandidato(this.nuevoCandidato).subscribe(
       data=>{
@@ -48,4 +81,5 @@ export class RegistroCandidatoComponent {
       }
     )
   }
+}
 }
--- frontend/src/app/auth/registroEmpresa/registro-empresa.component.ts
+++ frontend/src/app/auth/registroEmpresa/registro-empresa.component.ts
@@ -3,6 +3,7 @@ import { CreateEmpresaDto } from '../../models/create-empresa.dto';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import Swal from 'sweetalert2';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-registro-empresa',
@@ -19,15 +20,56 @@ export class RegistroEmpresaComponent  implements OnInit{
   telefono: number= 0; 
   cif: string= ""; 
   descripcion: string= ""
-
+  valid: boolean = true;
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private toastr: ToastrService,
 
   ) { }
   ngOnInit(): void {
   }
+
+  
   onRegister():void{
+    this.valid = true;
+
+    if (!this.empresa) {
+      this.toastr.error('La empresa es obligatoria', 'Error');
+      this.valid = false;
+    }
+    if (!this.nombre) {
+      this.toastr.error('El nombre es obligatorio', 'Error');
+      this.valid = false;
+    }
+    if (!this.apellidos) {
+      this.toastr.error('Los apellidos son', 'Error');
+      this.valid = false;
+    }
+    if (!this.password) {
+      this.toastr.error('La contraseña es obligatoria', 'Error');
+      this.valid = false;
+    }
+    if (!this.descripcion) {
+      this.toastr.error('La descripción es obligatoria', 'Error');
+      this.valid = false;
+    }
+    if (!this.cif || !/^\d{8}[A-HJ-NP-TV-Z]$/i.test(this.cif)) {
+      this.toastr.error('El DNI es obligatorio y debe ser válido', 'Error');
+      this.valid = false;
+    }
+
+    if (!this.telefono || !/^[679][0-9]{8}$/.test(this.telefono.toString())) {
+      this.toastr.error('El teléfono es obligatorio y debe ser válido', 'Error');
+      this.valid = false;
+    }    
+    
+    if (!this.email || !/\S+@\S+\.\S+/.test(this.email)) {
+      this.toastr.error('El email es obligatorio y debe ser válido', 'Error');
+      this.valid = false;
+    }
+
+    if (this.valid) {
     this.nuevaEmpresa = new CreateEmpresaDto(this.email,this.password,this.nombre,this.apellidos,this.empresa,this.telefono,this.cif,this.descripcion);
     this.authService.registroEmpresa(this.nuevaEmpresa).subscribe(
       data=>{
@@ -50,5 +92,5 @@ export class RegistroEmpresaComponent  implements OnInit{
       }
     )
   }
-
+}
 }
