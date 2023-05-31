f823ffe4dc2100e6b6e26ff3ab2cc2f6c9d136e6
raivac <raulivars1997@gmail.com>
2023-05-31 15:27:09 +0200

    a

--- frontend/src/env/env.ts
+++ frontend/src/env/env.ts
@@ -1,4 +1,4 @@
 export const env ={
-    authURL:'http://217.160.114.213:3000/auth/',
-    empleoURL:'http://217.160.114.213:3000/empleo'
+    authURL:'https://217.160.114.213:3000/auth/',
+    empleoURL:'https://217.160.114.213:3000/empleo'
 }
