2a2017382f195932f7360c3904f6907d2c877f95
raivac <raulivars1997@gmail.com>
2023-06-01 11:34:32 +0200

    a

--- frontend/src/env/env.ts
+++ frontend/src/env/env.ts
@@ -1,4 +1,4 @@
 export const env ={
-    authURL:'https://217.160.114.213:8080/auth/',
-    empleoURL:'https://217.160.114.213:8080/empleo'
+    authURL:'https://r-jobs.es/auth/',
+    empleoURL:'https://r-jobs.es/empleo'
 }
